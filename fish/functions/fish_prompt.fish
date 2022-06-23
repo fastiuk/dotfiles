32d9d640b28f4e845e3d45110829b2543d8abcfb
Yevhen Fastiuk <yfastiuk@nvidia.com>
2022-06-23 19:08:14 +0300

    Remove space before status

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -27,7 +27,7 @@ function fish_prompt --description 'Write out the prompt'
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    echo -n -s (prompt_login) ':' (set_color $color_cwd) (prompt_pwd) $normal " " $prompt_status $suffix " "
+    echo -n -s (prompt_login) ':' (set_color $color_cwd) (prompt_pwd) $normal $prompt_status $suffix " "
 end
 
 function fish_time_prompt -d "Print time in human readable format"
