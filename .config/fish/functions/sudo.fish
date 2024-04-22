04d3864d62cc14880225a064d6a70d9fea9178bf
Yevhen Fastiuk <yfastiuk@nvidia.com>
2024-04-22 17:07:01 +0300

    Update to new version of fish: 3.7.1

--- .config/fish/functions/sudo.fish
+++ .config/fish/functions/sudo.fish
@@ -1,7 +1,8 @@
-function sudo --description Replacement\ for\ Bash\ \'sudo\ !!\'\ command\ to\ run\ last\ command\ using\ sudo.
-if test "$argv" = !!
-eval command sudo $history[1]
-else
-command sudo $argv
-end
+function sudo --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    if test "$argv" = !!
+        echo sudo $history[1]
+        eval command sudo $history[1]
+    else
+        command sudo $argv
+    end
 end
